1b10c4821fa3360bf8c54c70bb7c0b5a2dac0400
salamshakur <salamshakur@knights.ucf.edu>
2019-01-23 02:32:19 -0500

    V2.0 - Added a mode

--- stats.py
+++ stats.py
@@ -1,24 +1,24 @@
 # Open text file containing values
 file = open("set.txt", "r")
 
-# Create set
-set = []
+# Create list
+list = []
 
 # Insert values into set
 for line in file:
     x = int(line)
-    set.append(x)
+    list.append(x)
 
 # Sort the set
-set.sort()
+list.sort()
 
 # Get sum of set values
 sum = 0
-for num in set:
+for num in list:
     sum+=num
 
 # Get mean
-n    = len(set)
+n    = len(list)
 mean = sum/n
 
 # Get median
@@ -29,9 +29,35 @@ median = 0
 if (n % 2) is not 0:
     i = (n + 1)/2 - 1
     i = round(i)
-    median = set[i]
+    median = list[i]
 else:
     i = (n + 1)/2 - 1
     left   = int(i)
     right  = round(i)
-    median = (set[left] + set[right])/2
+    median = (list[left] + list[right])/2
+
+# Convert list into a distinct set
+set = set(list)
+
+# Create another list to hold frequencies of values original list
+freq = []
+
+# Get frequencies of each value in list using the set
+for num in set:
+    x = list.count(num)
+    freq.append(x)
+
+# Find the highest frequency
+max = max(freq)
+
+# Convert set back into list for indexing
+distinct = []
+for num in set:
+    distinct.append(num)
+
+# Get mode
+mode = 0
+if freq.count(max) > 1:
+    mode = 0
+else:
+    mode = distinct[max]
